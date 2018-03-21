42f1e9c9ea8b6ab3ac3d621f2f8a8579a2df35b0
Philip Kalela <PhilSkiiiwalker@Philips-MacBook-Pro.local>
2018-03-21 14:31:38 +0300

    [fix] allow the whitenoise object to collect static

---
+++ staticfiles/js/welcome.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+
+    var input = document.getElementById("pitch-url-input");
+    input.addEventListener("click", function() {
+        this.select();
+    });
+
+}, 1000);
