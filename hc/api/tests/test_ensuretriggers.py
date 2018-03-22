5c5330951e2967bc48690edc475d68a0d5053e66
Felix Wambiri <FelixWambiri>
2018-03-22 22:29:11 +0300

    [FIX] Fix failing tests

--- hc/api/tests/test_ensuretriggers.py
+++ hc/api/tests/test_ensuretriggers.py
@@ -19,11 +19,12 @@ class EnsureTriggersTestCase(TestCase):
         check.save()
         check.refresh_from_db()
         assert check.alert_after is not None
-        ### The above assert fails. Make it pass
+        # Test seems to be ok
 
         alert_after = check.alert_after
 
         check.last_ping += timedelta(days=1)
         check.save()
         check.refresh_from_db()
-        ### Assert that alert_after is lesser than the check's alert_after 
+        # Assert that alert_after is lesser than the check's alert_after
+        assert(alert_after < check.alert_after)
