5603a642bcd6a4f7b780d24c6cb385f68b767218
meshnesh <tonnie.nesh@gmail.com>
2018-03-22 22:34:09 +0300

    [Feature] Add test to assert and check alice && test api in requests

--- hc/api/tests/test_list_checks.py
+++ hc/api/tests/test_list_checks.py
@@ -7,6 +7,7 @@ from hc.test import BaseTestCase
 
 
 class ListChecksTestCase(BaseTestCase):
+    URL = "/api/v1/checks/"
 
     def setUp(self):
         super(ListChecksTestCase, self).setUp()
@@ -40,7 +41,46 @@ class ListChecksTestCase(BaseTestCase):
 
         checks = {check["name"]: check for check in doc["checks"]}
         ### Assert the expected length of checks
-        ### Assert the checks Alice 1 and Alice 2's timeout, grace, ping_url, status,
+        self.assertEqual(len(checks), 2)
+        ### Assert the checks Alice 1 and  #  Alice 2's timeout, grace, ping_url, status,
+        ### Assert the checks Alice 1
+
+        Alice_1 = checks["Alice 1"]
+        check_1= dict(
+            timeout=Alice_1["timeout"],
+            grace=Alice_1["grace"],
+            status=Alice_1["status"],
+            last_ping=Alice_1["last_ping"],
+            n_pings=Alice_1["n_pings"],
+            ping_url=Alice_1["ping_url"]
+        )
+        expected_results = dict(
+            timeout=3600,
+            grace=900,
+            status="new",
+            last_ping=self.a1.last_ping.isoformat(),
+            n_pings=1,
+            ping_url=str(self.a1.url()),
+        )
+        self.assertEqual(check_1, expected_results)
+
+        #  Alice 2's timeout, grace, ping_url, status,
+        Alice_2 = checks["Alice 2"]
+        check_2= dict(
+            timeout=Alice_2["timeout"],
+            grace=Alice_2["grace"],
+            status=Alice_2["status"],
+            last_ping=Alice_2["last_ping"],
+            ping_url=Alice_2["ping_url"]
+        )
+        expected_results = dict(
+            timeout= 86400,
+            grace=3600,
+            status="up",
+            last_ping=self.a2.last_ping.isoformat(),
+            ping_url=str(self.a2.url()),
+        )
+        self.assertEqual(check_2, expected_results)
         ### last_ping, n_pings and pause_url
 
     def test_it_shows_only_users_checks(self):
@@ -54,3 +94,12 @@ class ListChecksTestCase(BaseTestCase):
             self.assertNotEqual(check["name"], "Bob 1")
 
     ### Test that it accepts an api_key in the request
+    def test_it_accepts_api_key_in_request(self):
+        payload = json.dumps({"api_key": "abc"})
+        r = self.client.generic(
+            'GET', self.URL,
+            payload,
+            content_type="application/json"
+        )
+
+        self.assertEqual(r.status_code, 200)
