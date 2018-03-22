dfb0747e79afcf48387d6385d817028e19995eaf
meshnesh <tonnie.nesh@gmail.com>
2018-03-22 22:41:06 +0300

    [Feature] Update 'test_it_works' && Add 'test_only_allows_post'

--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -13,6 +13,7 @@ class PauseTestCase(BaseTestCase):
                              HTTP_X_API_KEY="abc")
 
         ### Assert the expected status code and check's status
+        self.assertEqual(r.status_code, 200)
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -25,3 +26,24 @@ class PauseTestCase(BaseTestCase):
         self.assertEqual(r.status_code, 400)
 
         ### Test that it only allows post requests
+
+    def test_it_only_allows_post_requests(self):
+        check = Check(user=self.bob, status="up")
+        check.save()
+
+        url = "/api/v1/checks/%s/pause" % check.code
+        r = self.client.post(url, "", content_type="application/json",
+                             HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 400)
+
+        r = self.client.get(
+            url, content_type='application/json', HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
+        r = self.client.put(
+            url, '', content_type='application/json', HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
+        r = self.client.delete(
+            url, content_type='application/json', HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
