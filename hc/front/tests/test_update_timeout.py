27a54aec97cd47b107195a94311ab61627aa5017
Felix Wambiri <FelixWambiri>
2018-03-29 00:18:35 +0300

    [ADD] Add tests for nag timeout

--- hc/front/tests/test_update_timeout.py
+++ hc/front/tests/test_update_timeout.py
@@ -11,7 +11,7 @@ class UpdateTimeoutTestCase(BaseTestCase):
 
     def test_it_works(self):
         url = "/checks/%s/timeout/" % self.check.code
-        payload = {"timeout": 3600, "grace": 60}
+        payload = {"timeout": 3600, "grace": 60, "nag": 3600}
 
         self.client.login(username="alice@example.org", password="password")
         r = self.client.post(url, data=payload)
@@ -20,10 +20,11 @@ class UpdateTimeoutTestCase(BaseTestCase):
         check = Check.objects.get(code=self.check.code)
         assert check.timeout.total_seconds() == 3600
         assert check.grace.total_seconds() == 60
+        assert check.nag.total_seconds() == 3600
 
     def test_team_access_works(self):
         url = "/checks/%s/timeout/" % self.check.code
-        payload = {"timeout": 7200, "grace": 60}
+        payload = {"timeout": 7200, "grace": 60, "nag": 3600}
 
         # Logging in as bob, not alice. Bob has team access so this
         # should work.
@@ -35,7 +36,7 @@ class UpdateTimeoutTestCase(BaseTestCase):
 
     def test_it_handles_bad_uuid(self):
         url = "/checks/not-uuid/timeout/"
-        payload = {"timeout": 3600, "grace": 60}
+        payload = {"timeout": 3600, "grace": 60, "nag": 3600}
 
         self.client.login(username="alice@example.org", password="password")
         r = self.client.post(url, data=payload)
@@ -44,7 +45,7 @@ class UpdateTimeoutTestCase(BaseTestCase):
     def test_it_handles_missing_uuid(self):
         # Valid UUID but there is no check for it:
         url = "/checks/6837d6ec-fc08-4da5-a67f-08a9ed1ccf62/timeout/"
-        payload = {"timeout": 3600, "grace": 60}
+        payload = {"timeout": 3600, "grace": 60, "nag": 3600}
 
         self.client.login(username="alice@example.org", password="password")
         r = self.client.post(url, data=payload)
@@ -52,7 +53,7 @@ class UpdateTimeoutTestCase(BaseTestCase):
 
     def test_it_checks_ownership(self):
         url = "/checks/%s/timeout/" % self.check.code
-        payload = {"timeout": 3600, "grace": 60}
+        payload = {"timeout": 3600, "grace": 60, "nag": 3600}
 
         self.client.login(username="charlie@example.org", password="password")
         r = self.client.post(url, data=payload)
