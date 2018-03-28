76fb9d1979a14af36ed5ca1b7119b0513d886566
Felix Wambiri <FelixWambiri>
2018-03-28 20:15:21 +0300

    [NEW] Add new nag property

--- hc/api/schemas.py
+++ hc/api/schemas.py
@@ -4,6 +4,9 @@ check = {
         "tags": {"type": "string"},
         "timeout": {"type": "number", "minimum": 60, "maximum": 15552000},
         "grace": {"type": "number", "minimum": 60, "maximum": 15552000},
+        "timeout": {"type": "number", "minimum": 60, "maximum": 604800},
+        "grace": {"type": "number", "minimum": 60, "maximum": 604800},
+        "nag":{"type": "number", "minimum": 60, "maximum":604800},
         "channels": {"type": "string"}
     }
 }
