eb36999b1a2ce41f05f6609246d6295bcffe15bf
Felix Wambiri <FelixWambiri>
2018-04-05 23:46:07 +0300

    [REF] Edit names of form to bring uniformity

--- hc/blogs/forms.py
+++ hc/blogs/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from .models import BlogPostsCategory, BlogPosts, Comments
 
-class BlogCategoryForm(forms.ModelForm):
+class BlogPostsCategoryForm(forms.ModelForm):
     class Meta:
         model = BlogPostsCategory
         fields = ('title')
 
-class BlogForm(forms.ModelForm):
+class BlogPostsForm(forms.ModelForm):
     class Meta:
         model = BlogPosts
         fields = ('category', 'title','content')
