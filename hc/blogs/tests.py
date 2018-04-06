8f5e490e062c0ec7555be2fa3c7941cea13e8b34
Felix Wambiri <FelixWambiri>
2018-04-06 04:42:08 +0300

    [NEW] Add test for creation of category

---
+++ hc/blogs/tests.py
@@ -0,0 +1,29 @@
+from hc.test import BaseTestCase
+from django.contrib.auth.models import User
+from .models import BlogPosts, BlogPostsCategory, Comment
+from django.shortcuts import reverse
+from django.utils import timezone
+
+
+"""
+    All our tests will be in this file and first we initialize all the reusable variables in the setup function
+"""
+class BlogPostsCategories(BaseTestCase):
+    def setUp(self):
+        super(BlogPostsCategories, self).setUp()
+        self.client.login(username="alice@example.org", password="password")
+        self.category = BlogPostsCategory(title='Machine Learning')
+        self.category.save()
+        self.blog = BlogPosts(title='Basics', content='It is the very beginning', 
+                        category=self.category)
+        self.blog.save()
+
+
+
+    def test_create_category(self):
+        url = reverse('blog:hc-category')
+        data = {'create_category-title': ['read'], 'create_category': ['']}
+        response = self.client.post(url, data)
+        category = BlogPostsCategory.objects.filter(title='read').first()
+        self.assertEqual('read', category.title)
+
