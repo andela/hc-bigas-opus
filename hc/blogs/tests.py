fb6716d11880198af85fdc889a17c505c5c19734
Felix Wambiri <FelixWambiri>
2018-04-11 18:36:04 +0300

    [ADD] Add more tests to guide creation of blog views

--- hc/blogs/tests.py
+++ hc/blogs/tests.py
@@ -1,3 +1,6 @@
+from django.test import TestCase
+
+# Create your tests here.
 from hc.test import BaseTestCase
 from django.contrib.auth.models import User
 from .models import BlogPosts, BlogPostsCategory, Comment
@@ -10,27 +13,64 @@ from django.utils import timezone
 """
 class BlogPostsCategories(BaseTestCase):
     def setUp(self):
-        # super(BlogPostsCategories, self).setUp()
         self.client.login(username="alice@example.org", password="password")
-        self.category = BlogPostsCategory(title='YOYO')
+        self.category = BlogPostsCategory(title='Machine Learning')
         self.category.save()
-        self.blog = BlogPosts(title='Learning', content='You are accountable to yourself', 
+        self.blog = BlogPosts(title='Basics', content='It is the very beginning', 
                         category=self.category)
         self.blog.save()
-
-
-
     def test_create_category(self):
-        url = reverse('blog:hc-category')
+        url = reverse('blogs:hc-category')
         data = {'create_category-title': ['read'], 'create_category': ['']}
         response = self.client.post(url, data)
         category = BlogPostsCategory.objects.filter(title='read').first()
         self.assertEqual('read', category.title)
 
-    def test_create_blog_post(self):
-        url = reverse('blog:hc-category')
-        data = {'selectop': ['1'], 'title': ['read'], 'content': ['read'], 'create_blog': ['']}
-        response = self.client.post(url, data)
-        blog = Blog.objects.filter(title='read').first()
-        self.assertEqual('read', blog.title)
 
+    def test_home_page_returns_all_categories(self):
+        url = reverse('blogs:hc-category')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/view_blogs.html')
+
+
+    def test_delete_blog(self):
+        blog = BlogPosts.objects.filter(title='Basics').first()
+        url = reverse('blogs:hc-delete-blog', kwargs={'pk':blog.id})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_create_blog_form_invalid(self):
+        url = reverse('blogs:hc-category')
+        data = {'selectop': ['1'], 'title':[''], 'content': ['read'], 'create_blog': ['']}
+        response = self.client.post(url, data)
+        self.assertTemplateUsed(response, 'blog/create_categories_and_blogs.html')
+
+    def test_create_category_form_invalid(self):
+        url = reverse('blogs:hc-category')
+        data = {'create_category-title': [''], 'create_category': ['']}
+        response = self.client.post(url, data)
+        self.assertRedirects(response, '/blogs/')
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_if_get_redirect(self):
+        url = reverse('blogs:hc-category')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/view_blogs.html')
+
+    def test_no_blogs_to_display(self):
+        category = BlogPostsCategory(title='Reads')
+        category.save()
+        category1 = BlogPostsCategory.objects.get(title='Reads')
+        url = reverse('blogs:hc-category-blogs', kwargs={'category':category1.id})
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'blog/view_blogs.html')
+    
+    def test_view_blog_detail(self):
+        blog = BlogPosts.objects.filter(title='Basics').first()
+        url = reverse('blogs:hc-view-blog', kwargs={'pk':blog.id})
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'blog/view_posts.html')
