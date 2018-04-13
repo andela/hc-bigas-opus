f9a39ddcfcef712d57d78b4c0f3a928f499164e3
meshnesh <tonnie.nesh@gmail.com>
2018-04-13 11:35:22 +0300

    [Test] Add test to handdle many commads sent

--- hc/api/tests/test_sendalerts.py
+++ hc/api/tests/test_sendalerts.py
@@ -40,3 +40,14 @@ class SendAlertsTestCase(BaseTestCase):
         Command().handle_one(check)
 
     ### Assert when Command's handle many that when handle_many should return True
+    @patch("hc.api.management.commands.sendalerts.Command.handle_many")
+    def test_commands_handle_many(self, mock):
+        yesterday = timezone.now() - timedelta(days=1)
+        names = ["Check %d" % d for d in range(0, 1000)]
+        for name in names:
+            check = Check(user=self.alice, name=name)
+            check.alert_after = yesterday
+            check.status = "up"
+            check.save()
+        result = Command().handle_many()
+        assert result, "handle_many should return True"
